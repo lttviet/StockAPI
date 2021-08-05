582545eb212da98b3c66091e596575c492a21a6f
lttviet <lttviet@gmail.com>
2021-08-04 21:35:59 -0400

    Move client app url to appsettings config

--- Startup.cs
+++ Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using Microsoft.Extensions.Configuration;
 
 using StockBE.Hubs;
 using StockBE.Services;
@@ -14,16 +15,25 @@ namespace StockBE
   {
     private readonly string LocalCorsPolicy = "LocalCorsPolicy";
 
+    public Startup(IConfiguration configuration)
+    {
+      Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
     // This method gets called by the runtime. Use this method to add services to the container.
     // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
     public void ConfigureServices(IServiceCollection services)
     {
+      string clientAppURL = Configuration["AngularURL"];
+
       services.AddCors(options =>
       {
         options.AddPolicy(LocalCorsPolicy,
                           builder =>
                           {
-                            builder.WithOrigins("http://localhost:4200")
+                            builder.WithOrigins(clientAppURL)
                               .AllowAnyMethod()
                               .AllowAnyHeader()
                               .AllowCredentials();
