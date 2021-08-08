44ae08a32de3f0d8794242285dc9876c8c0f0f8f
lttviet <lttviet@gmail.com>
2021-08-08 17:40:23 -0400

    Add Forwarded Headers middleware

--- Startup.cs
+++ Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.HttpOverrides;
 
 using StockBE.Hubs;
 using StockBE.Services;
@@ -61,6 +62,13 @@ namespace StockBE
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
+      app.UseForwardedHeaders(
+        new ForwardedHeadersOptions
+        {
+          ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+        }
+      );
+
       if (env.IsDevelopment())
       {
         app.UseDeveloperExceptionPage();
